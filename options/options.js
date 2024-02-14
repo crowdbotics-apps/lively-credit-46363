ffc3342f477b1c3cc73fe9244d99d7ef6d38a927
Crowdbotics <team@crowdbotics.com>
2024-02-14 22:21:40 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lively-credit-46363",
+  url: "https://lively-credit-46363.botics.co",
+  api: "https://lively-credit-46363.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
